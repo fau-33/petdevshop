31c6b6af9f10af0384cf88e11bda75f14188c82f
Flávio Félix <fau_felix30@hotmail.com>
2026-07-09 16:51:15 -0300

    Configurando os controller e routes

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import { fileURLToPath } from "url";
+import mainRoutes from "./routes/index.js";
 
 dotenv.config();
 
 const server = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 server.set("view engine", "mustache");
 server.set("views", path.join(__dirname, "views"));
@@ -14,5 +18,10 @@ server.engine("mustache", mustache());
 server.use(express.static(path.join(__dirname, "../public")));
 
 // Rotas
+server.use(mainRoutes);
 
-server.listen(process.env.PORT);
+server.use((req, res) => {
+  res.send("Página não encontrada!");
+});
+
+server.listen(process.env.PORT || 3000);
