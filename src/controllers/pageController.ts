1010bd8abc49dd02b7e73d0d9c92eb74247f1228
Flávio Félix <fau_felix30@hotmail.com>
2026-07-09 20:06:31 -0300

    Repartindo as páginas

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,22 +1,37 @@
 import type { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("home no controller");
-  //res.render('pages/page');
+  res.render("pages/page", {
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
 
 export const dogs = (req: Request, res: Response) => {
-  res.send("dogs no controller");
-  //res.render('pages/dogs');
+  res.render("pages/page", {
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.jpg",
+    },
+  });
 };
 
 export const cats = (req: Request, res: Response) => {
-  res.send("cats no controller");
-
-  //res.render('pages/cats');
+  res.render("pages/page", {
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
 
 export const fishes = (req: Request, res: Response) => {
-  res.send("fishes no controller");
-  //res.render('pages/fishes');
+  res.render("pages/page", {
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
 };
